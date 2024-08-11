45cb587a719220d0ccc9ba3952c98d4162da8fd0
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-08-11 11:34:55 -0300

    Adds post user

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html')
 });
 
+let users = [];
 
+app.use(express.urlencoded());
 
+app.post('/api/users', (req, res) => {
+    const user = req.body.username;
+    users.push(user)
+    res.json({
+        username: user,
+        _id: users.indexOf(user)
+    })
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+    console.log('Your app is listening on port ' + listener.address().port)
 })
